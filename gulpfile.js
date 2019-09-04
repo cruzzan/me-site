c4a842c59fcfaf7fea3a88ba5cf1c985682b7e93
Sebastian Hultstrand <sebastian@storm-net.se>
2019-09-05 01:26:44 +0200

    Adds fa icons instead of the built in ones.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var cleancss = require('gulp-clean-css');
 
-gulp.task('default', ['less-to-css', 'minify-css']);
+gulp.task('default', ['less-to-css', 'minify-css', 'font-awesome']);
 
 gulp.task('less-to-css', function () {
 	gulp.src('./site/less/*.less')
@@ -14,4 +14,11 @@ gulp.task('minify-css', function () {
 	gulp.src('./site/dist/*.css')
 		.pipe(cleancss())
 		.pipe(gulp.dest('./site/dist/min'));
+});
+
+gulp.task('font-awesome', function () {
+	gulp.src('./node_modules/@fortawesome/fontawesome-free/css/all.css')
+		.pipe(gulp.dest('./site/dist/font-awesome/css/'));
+	gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/*')
+		.pipe(gulp.dest('./site/dist/font-awesome/webfonts/'));
 });
